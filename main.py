482a8e859f3b29c4e64911645da7f9c56370cff2
kaiky nonato <kaikynonato288br@gmail.com>
2025-10-24 06:28:21 -0300

    ajustes

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 from fastapi import FastAPI
 from api.routes import company_routes
-# Importar novas rotas
 from api.routes import type_product_routes
 from api.routes import supplier_routes
 from api.routes import product_routes
 from api.routes import stock_routes
 
 
-app = FastAPI(
-    title="API de Sistema de Estoque",
-    description="API REST para gerenciamento de empresas, produtos e estoque.",
-    version="1.0.0"
-)
+app = FastAPI()
 
-# Incluir todos os roteadores
 app.include_router(company_routes.router)
 app.include_router(type_product_routes.router)
 app.include_router(supplier_routes.router)
